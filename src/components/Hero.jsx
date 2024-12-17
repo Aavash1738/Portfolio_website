512eb8390c07c0d4f03d60e723f915bba95bff1d
Aavash1738 <bhattaraiaavash1@gmail.com>
2024-12-17 19:22:42 +0545

    Added CV, made download available, added motion

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { HERO_CONTENT } from "../constants";
 import profilePic from "../assets/profile.jpeg";
 import { motion, AnimatePresence } from "framer-motion";
+import { FaFileArrowDown } from "react-icons/fa6";
 
 const container = (delay) => ({
   hidden: { x: -100, opacity: 0 },
@@ -49,10 +50,42 @@ const Hero = () => {
               variants={container(1)}
               initial="hidden"
               animate="visible"
-              className="my-2 max-w-xl py-6 font-light tracking-lighter"
+              className="mt-2 max-w-xl pt-6 pb-4 font-light tracking-lighter"
             >
               {HERO_CONTENT}
             </motion.p>
+            <motion.div
+              className="max-w-xl py-4 font-light trackin-lighter text-3xl"
+              variants={container(1)}
+              initial="hidden"
+              animate="visible"
+              title="Click here to download CV"
+            >
+              <a href="/cv.pdf" target="_blank">
+                <div className="flex gap-3 justify-center items-center group">
+                  <p className="font-light">
+                    Download <span className="text-neutral-500">CV</span>
+                  </p>
+                  <motion.div
+                    className="flex justify-center items-center"
+                    animate={{
+                      y: ["0%", "-20%", "0%"],
+                      rotate: [0, 10, -10, 0],
+                    }}
+                    transition={{
+                      duration: 0.8,
+                      repeat: Infinity,
+                      repeatType: "loop",
+                      ease: "easeInOut",
+                      delay: 5,
+                      repeatDelay: 5,
+                    }}
+                  >
+                    <FaFileArrowDown className="group-hover:text-[#50be63] " />
+                  </motion.div>
+                </div>
+              </a>
+            </motion.div>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
